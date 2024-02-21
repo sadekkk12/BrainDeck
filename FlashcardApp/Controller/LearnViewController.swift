df033a404cfa2acccb5f95926a672648c267b98a
sadekkk12 <sade0027@stud.kea.dk>
2024-02-21 17:33:07 +0100

    Test commit

--- FlashcardApp/Controller/LearnViewController.swift
+++ FlashcardApp/Controller/LearnViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 class LearnViewController: UIViewController {
     
 }
-//TODO Redo layout constraints
+//TODO Redo layout constraintsjj
 //TODO refactor Stacks and improve visually
 //TODO improve text readability for reader, check guidelines for whats best
 //TODO apply custom color schema
